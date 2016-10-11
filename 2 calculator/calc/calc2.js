a0595045af48568cc1909fd8a5182bcc857ed613
Himanshu Chandra <himanshu.chandra@outlook.com>
2016-10-11 11:08:17 +0530

    Add files via upload

---
+++ 2 calculator/calc/calc2.js
@@ -0,0 +1,15 @@
+
+var object={
+    
+    subtract:function sub(x,y){
+        return x-y;
+    },
+    
+    add:function ad(x,y){
+        return x+y;
+    }   
+
+}
+//module.exports = add;
+module.exports=object;
+    
---
+++ 2 calculator/calc2/caller2.js
@@ -0,0 +1,5 @@
+var object = require("../calc/calc2");
+console.log(typeof object);
+//console.log(addition(100,200));
+console.log(object.add(100,200));
+console.log(object.subtract(100,200));
